a120493138d80d071205ce08e206bf1cb59a205b
Brayden <Braydenlwong@gmail.com>
2023-08-10 10:38:23 -0600

    dockerized applications

--- hire/api/apps/gateway/src/gateway.module.ts
+++ hire/api/apps/gateway/src/gateway.module.ts
@@ -1,10 +1,28 @@
 import { Module } from "@nestjs/common";
-import { GatewayController } from "./gateway.controller";
-import { GatewayService } from "./gateway.service";
+import { ClientsModule, Transport } from "@nestjs/microservices";
+import { AUTH_SERVICE } from "@app/common/constants";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
-  imports: [],
-  controllers: [GatewayController],
-  providers: [GatewayService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `./apps/gateway/.env.local.${process.env.NODE_ENV}`,
+    }),
+    ClientsModule.registerAsync([
+      {
+        name: AUTH_SERVICE,
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (config: ConfigService) => ({
+          transport: Transport.TCP,
+          options: {
+            host: config.get("AUTH_HOST"),
+            port: config.get<number>("AUTH_PORT"),
+          },
+        }),
+      },
+    ]),
+  ],
 })
 export class GatewayModule {}
--- hire/api/apps/gateway/src/main.ts
+++ hire/api/apps/gateway/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from "@nestjs/core";
 import { GatewayModule } from "./gateway.module";
+import { ConfigService } from "@nestjs/config";
+import { Logger } from "@nestjs/common";
 
 async function bootstrap() {
+  const logger = new Logger("Gateway Bootstrap");
   const app = await NestFactory.create(GatewayModule);
-  await app.listen(3000);
+  const config = app.get<ConfigService>(ConfigService);
+  const PORT = config.get<number>("PORT");
+
+  await app.listen(PORT, async () =>
+    logger.log(`Gateway is listening @${await app.getUrl()}`),
+  );
 }
 bootstrap();
